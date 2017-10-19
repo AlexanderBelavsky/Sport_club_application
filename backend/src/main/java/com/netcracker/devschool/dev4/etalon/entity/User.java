9347ec232a7447181b7428cabfab14626695d7d2
Alexander Belavsky <a.belavsky97@gmail.com>
2017-10-19 21:15:30 +0300

    First entity

--- backend/src/main/java/com/netcracker/devschool/dev4/etalon/entity/User.java
+++ backend/src/main/java/com/netcracker/devschool/dev4/etalon/entity/User.java
@@ -1,8 +1,41 @@
 package com.netcracker.devschool.dev4.etalon.entity;
 
-import org.hibernate.annotations.GenericGenerator;
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
+    @Id
+    private String username;
+
+    private String password;
+
+    private int enabled;
+
+    public int getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(int enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
